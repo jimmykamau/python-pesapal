c819dfa2f995b4de9a72f8f94d32b52b3ab105c7
Mitchel Kelonye <kelonyemitchel@gmail.com>
2014-01-09 10:46:12 +0300

    Release 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pesapal',
-    version='0.0.4',
+    version='0.0.5',
     description='Pesapal API python library.',
     author='Mitchel Kelonye',
     author_email='kelonyemitchel@gmail.com',
